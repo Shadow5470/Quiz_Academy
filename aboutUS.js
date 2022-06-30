042c9ae669bed4dea17e2f0f0bea286c2513df1e
Shadow5470 <69870457+Shadow5470@users.noreply.github.com>
2022-06-30 03:52:48 -0400

    basics of about us

--- aboutUS.js
+++ aboutUS.js
@@ -1,4 +1,4 @@
-var newTags = {"Tutoring":"https://bobbyhadz.com/blog/javascript-create-element-with-class","Friv":"https://www.friv.com/","About Us":"https://shadow5470.github.io/Quiz_Academy/aboutUS"}
+var newTags = {"Tutoring":"https://bobbyhadz.com/blog/javascript-create-element-with-class","Friv":"https://www.friv.com/","About Me":"aboutme"}
 var topLinks = document.getElementsByTagName("a");
 var links = document.getElementById("tallLine");
 for(var keyTag in newTags){
